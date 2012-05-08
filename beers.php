1b144d63d7b9d468af2b1ec116023402af93aa56
Benjamin Young <byoung@bigbluehat.com>
2012-05-08 16:26:46 -0400

    added BrowserID login; beers added to user doc
    User docs are stored as sha1($email) for some level of privacy.
    The list of beers is stored as a pipe (|) delimited list of beer
    doc IDs.

--- beers.php
+++ beers.php
@@ -16,16 +16,22 @@ $app->get('/beers/', function() use ($app, $cb) {
 });
 
 $app->get('/beers/:id', function($id) use ($app, $cb) {
-  $beer = json_decode($cb->get('beer_' . str_replace(' ', '_', urldecode($id))), true);
-  if ($beer !== null) {
-    if (isset($beer['brewery'])) {
-      $beer['brewery_url'] = breweryUrl($beer['brewery']);
-    }
-    $app->view()->appendData($beer);
-    $content = $app->view()->render('beer.mustache');
-    $app->render('layout.mustache', compact('content'));
+  if (!isset($_SESSION['email'])) {
+    $app->response()->status(401);
   } else {
-    $app->notFound();
+    $beer_id = 'beer_' . str_replace(' ', '_', urldecode($id));
+    $beer = json_decode($cb->get($beer_id), true);
+    if ($beer !== null) {
+      $cb->append(sha1($_SESSION['email']), $beer_id . '|');
+      if (isset($beer['brewery'])) {
+        $beer['brewery_url'] = breweryUrl($beer['brewery']);
+      }
+      $app->view()->appendData($beer);
+      $content = $app->view()->render('beer.mustache');
+      $app->render('layout.mustache', compact('content'));
+    } else {
+      $app->notFound();
+    }
   }
 });
 
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 // require stuffs
+require 'vendors/Resty/Resty.php';
 require 'vendors/Slim/Slim/Slim.php';
 require 'vendors/Slim-Extras/Views/MustacheView.php';
 
@@ -13,6 +14,8 @@ $app->view()->appendData(array(
   'base_url' => $env['SCRIPT_NAME'],
   'current_url' => $env['PATH_INFO']
 ));
+
+$app->add(new Slim_Middleware_SessionCookie());
 
 // Setup Couchbase connected objects
 try {
@@ -28,6 +31,47 @@ $app->get('/', function () use ($app) {
   $content = $app->view()->render('index.mustache');
   $app->render('layout.mustache', compact('content') + array('on_index' => true));
 });
+
+// GET BrowserID verification
+$app->post('/browserid/login', function () use ($app, $cb) {
+  header('Content-Type: application/json');
+  $resty = new Resty();
+  $resty->debug(true);
+  $assertion=$app->request()->post('assertion');
+  // get the POSTed assertion
+  $post_data = array('assertion' => $assertion, 'audience' => $_SERVER['SERVER_NAME']);
+  // SERVER is my site's hostname
+  $resty->setBaseURL('https://browserid.org/');
+  // This makes a post request to browserid.org
+  $r = $resty->post('verify',$post_data);
+
+  if ($r['body']->status == 'okay') {
+    // This logs the user in if we have an account for that email address,
+    // or creates it otherwise
+    //$email = sha1($r['body']['email']);
+    $email = $_SESSION['email'] = $r['body']->email;
+    if ($cb->get(sha1($email)) === null) {
+      $cb->set(sha1($email), '');
+    }
+    echo json_encode($email);
+  } else {
+    $msg = 'Could not log you in';
+    $status = false;
+    echo json_encode(array('message'=>$msg,'status'=>$status));
+  }
+});
+
+$app->post('/browserid/logout', function() use ($app) {
+  unset($_SESSION['email']);
+});
+
+$app->get('/browserid/whoami', function() use ($app) {
+  header('Content-Type: application/json');
+  if (isset($_SESSION['email'])) {
+    echo json_encode($_SESSION['email']);
+  }
+});
+
 // beer routes
 require_once 'beers.php';
 // brewery routes
